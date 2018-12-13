4433939081a7961c2c7a15faaa8db4508ff59e6d
twostarxx <xingxin.hsing@gmail.com>
2018-12-13 15:12:17 +0800

    + MessageServiceImpl

---
+++ neo4j/src/main/java/com/project_management/shoppingweb/service/Impl/MessageServiceImpl.java
@@ -0,0 +1,22 @@
+package com.project_management.shoppingweb.service.Impl;
+
+import com.project_management.shoppingweb.constant.HttpResponseConstants;
+import com.project_management.shoppingweb.pojo.nodeEntity.Message;
+import com.project_management.shoppingweb.pojo.vo.RequestResultVO;
+import com.project_management.shoppingweb.repository.MessageRepository;
+import com.project_management.shoppingweb.service.MessageService;
+import com.project_management.shoppingweb.service.common.ResultBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageServiceImpl implements MessageService {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Override
+    public RequestResultVO insert(Message message) {
+        messageRepository.save(message);
+        return ResultBuilder.buildSuccessResult(HttpResponseConstants.Public.SUCCESS_200);
+    }
+}
